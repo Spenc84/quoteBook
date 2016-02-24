f92429cbfe65375bb57a61c84bcb56a5fb905dc4
Spencer Powell <spenc84@gmail.com>
2016-02-24 00:15:18 -0600

    Black Diamond Complete

--- js/dataService.js
+++ js/dataService.js
@@ -1,5 +1,9 @@
 angular.module('quoteBook').service('dataService', function(){
-  var quotes = [
+
+  if(localStorage.quoteBook){
+    var quotes = JSON.parse(localStorage.quoteBook).quoteArray;
+  }
+  else quotes = [
     { text: 'Life isn\'t about getting and having, it\'s about giving and being.', author: 'Kevin Kruse'},
     { text: 'Whatever the mind of man can conceive and believe, it can achieve', author: 'Napoleon Hill'},
     { text: 'Strive not to be a success, but rather to be of value.', author: 'Albert Einstein'},
@@ -8,9 +12,17 @@ angular.module('quoteBook').service('dataService', function(){
     { text: 'Life is what happens to you while you\'re busy making other plans.', author: 'John Lennon'},
     { text: 'What even is a jQuery?', author: 'Tyler S. McGinnis'}
   ];
+  $(window).unload(function(){
+    localStorage.quoteBook = JSON.stringify({ quoteArray: quotes });
+  });
 
   this.getData = function(){ return quotes; };
-  this.addData = function(obj){ if(obj.text && obj.author) quotes.push(obj); else alert("Bad data pushed to addData()"); };
+  this.addData = function(obj){
+    if(obj.text && obj.author){
+      quotes.push(obj);
+    }
+    else alert("Bad data pushed to addData()");
+  };
   this.removeData = function(str){
     str = str.toLowerCase();
     for(var i = quotes.length-1; i >= 0; i--) {
@@ -19,5 +31,7 @@ angular.module('quoteBook').service('dataService', function(){
       }
     }
   };
+  // $(document).ready(function(){
 
+  // });
 });
